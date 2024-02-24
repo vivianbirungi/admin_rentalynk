445c65a23b819c4febf6276ba9d4497c5f2bfc37
vivian Birungi <viviangal20@gmail.com>
2024-02-24 13:28:47 +0300

    ehanced designs

--- app/ui/dashboard/notification/add/addNotification.jsx
+++ app/ui/dashboard/notification/add/addNotification.jsx
@@ -1,29 +1,42 @@
 import { useState } from "react";
-import SelectItem from '../../select/select';
-import styles from './addNotification.module.css';
+import SelectItem from "../../select/select";
+import styles from "./addNotification.module.css";
 const AddNotification = () => {
-    const [type, setType]= useState('landlord');
-  const [message, setMessage] = useState('');
+  const [type, setType] = useState("landlord");
+  const [message, setMessage] = useState("");
   return (
-    <div>
-       <form className={styles.formContainer} >
-       
-          <SelectItem selectedValue={type} values={['landlord','tenant']} handleSelect={(e)=>{setType(e)}}/>
-         
-          <SelectItem selectedValue={type} values={['User1','User2']} handleSelect={(e)=>{setType(e)}}/>
-          
-          <textarea
-            className={styles.message}
-            value={message}
-            placeholder=" Message"
-            onChange={(e) => setMessage(e.target.value)}
-            rows={4} // Adjust the number of rows as needed
-          />
-     
-      <button className={styles.button} type="submit">Send Message</button>
-        </form>
-    </div>
-  )
-}
+    <div className={styles.container}>
+      <form className={styles.formContainer}>
+        <SelectItem
+          selectedValue={type}
+          values={["landlord", "tenant"]}
+          handleSelect={(e) => {
+            setType(e);
+          }}
+        />
 
-export default AddNotification
+        <SelectItem
+          selectedValue={type}
+          values={["User1", "User2"]}
+          handleSelect={(e) => {
+            setType(e);
+          }}
+        />
+
+        <textarea
+          className={styles.message}
+          value={message}
+          placeholder=" Message"
+          onChange={(e) => setMessage(e.target.value)}
+          rows={4} // Adjust the number of rows as needed
+        />
+
+        <button className={styles.button} type="submit">
+          Send Message
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddNotification;
